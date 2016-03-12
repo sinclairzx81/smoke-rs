47de0fe998bf4065d1196ba52e56904c4e9b5092
sinclairzx81 <haydn.developer@gmail.com>
2016-03-13 05:12:18 +1300

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,8 @@
 mod smoke;
-use smoke::async::Stream;
 
-#[derive(Debug)]
-enum Foo {
-  Number(i32),
-  Word(&'static str)
-}
-
-fn numbers() -> Stream<Foo> {
-  Stream::new(|sender| {
-    try! (sender.send(Foo::Number(1)) );
-    try! (sender.send(Foo::Number(2)) );
-    try! (sender.send(Foo::Number(3)) );
-    try! (sender.send(Foo::Number(4)) );
-    Ok(())
-  }) 
-}
-
-fn words() -> Stream<Foo> {
-  Stream::new(|sender| {
-      "the quick brown fox jumps over the lazy dog"
-        .split(" ")
-        .map(|n| sender.send(Foo::Word(n)))
-        .last()
-        .unwrap()
-  }) 
-}
+#[allow(unused_imports)]
+use smoke::async::{Task, Stream};
 
 fn main() {
-  // mux
-  let stream = Stream::mux(vec![
-      numbers(), 
-      words()
-      ]);
   
-  // demux
-  for foo in stream.recv() {
-      match foo {
-        Foo::Number(n) => 
-          println!("number -> {}", n),
-        Foo::Word(n) => 
-          println!("word -> {}", n)
-      }
-  }
 }
