1e8a4f1de1005b91776cad42bf5c940ce9195b58
sinclairzx81 <haydn.developer@gmail.com>
2016-03-13 13:27:50 +1300

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,8 @@
 mod smoke;
-use smoke::async::Task;
-use std::thread;
-use std::time::Duration;
 
-fn add(a: i32, b: i32) -> Task<i32, ()> {
-  Task::new(move || {
-    thread::sleep(Duration::from_secs(1));
-    Ok(a + b)
-  })
-}
+#[allow(unused_imports)]
+use smoke::async::{Task, Stream};
 
 fn main() {
-   let result = Task::all(vec![
-     add(10, 20),
-     add(20, 30),
-     add(30, 40)
-   ]).sync().unwrap(); 
-   
-   // [30, 50, 70]
-   println!("{:?}", result); 
+  
 }
