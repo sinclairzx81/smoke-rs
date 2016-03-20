156089d09139f9263c93b5ceb643d91151dc8d34
sinclairzx81 <haydn.developer@gmail.com>
2016-03-20 21:23:46 +1300

    workbench checkin

--- examples/bench.rs
+++ examples/bench.rs
@@ -1,11 +1,43 @@
 extern crate smoke;
 
 use smoke::async::Task;
+use smoke::async::Stream;
+use std::thread;
+use std::net::{TcpListener, TcpStream};
 
-fn hello() -> Task<&'static str> {
-  Task::delay(100).map(|_| "hello")
+/// simple tcp server.
+fn server(local: &'static str) -> Stream<TcpStream> {
+  Stream::new(move |sender| {
+    let listener = TcpListener::bind(local).unwrap();
+    for stream in listener.incoming() {
+        try!(sender.send(stream.unwrap()))
+    } Ok(())
+  })
+}
+
+/// simple tcp client stream.
+fn client(remote: &'static str) -> Task<TcpStream> {
+  Task::new(move |sender| {
+    let stream = TcpStream::connect(remote).unwrap();
+    sender.send(stream)
+  })
 }
 
 fn main() {
-  println!("{:?}", hello().wait());
+  
+  // server handler....
+  thread::spawn(|| {
+    for stream in server("localhost:5000").read(0) {
+        println!("server: new stream");
+    }   
+  });
+  
+  // clients..
+  let tasks = (0..50).map(|n| client("localhost:5000")).collect::<Vec<_>>();
+  Task::all(4, tasks).async(|streams| {
+     println!("clients connected: {:?}", streams);
+  }).wait();
+  
+  thread::sleep_ms(100);
+
 }
